a00adab102454bfc5d1a20b21563a268ca9d26f8
Vaibhav <rawatvaibhav42@gmail.com>
2023-11-02 19:28:12 +0530

    edited contact us page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,28 +2,26 @@ import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
 import "../styles/responsive.css";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
     <Layout title={"Contact us"}>
-      <div
-        className="text-center row contactus "
-        style={{ fontFamily: "Calisto MT, serif" }}
-      >
-        <div className="col-md-4">
+      <div className="container" style={{ fontFamily: "Calisto MT, serif" }}>
+        <div className="zeeta-box">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            If you have any query or need info about product feel free to call
-            anytime, we are 24X7 available.
+            If you have any query or need info about product feel free to reach
+            anytime.
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@booksellf.com
+            <BiMailSend /> : rawatsuraj9867@gmail.com
           </p>
           <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
-          </p>
-          <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiPhoneCall /> :{" "}
+            <a href="https://www.linkedin.com/in/vaibhav-singh-rawat-933094234/">
+              Linkedin
+            </a>
           </p>
         </div>
       </div>
