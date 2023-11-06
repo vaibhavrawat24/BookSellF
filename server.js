56474e7ed99715e82f8fc7e8fc433eff6bf0cf35
Vaibhav <rawatvaibhav42@gmail.com>
2023-11-06 22:44:15 +0530

    added deployement code

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import authorRoutes from "./routes/authorRoutes.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -18,14 +19,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/author", authorRoutes);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to my book website</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
